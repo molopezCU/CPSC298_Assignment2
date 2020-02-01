1371bbcda031b125920dffdb2ea826a31d80b685
molopezCU <molopez@chapman.edu>
2020-02-01 00:43:11 +0000

    Problem1(intArray) submission.

---
+++ Problem1(intArray)/Problem1(intArray).cpp
@@ -0,0 +1,35 @@
+/*
+Moises Lopez
+229564
+molopez@chapman.edu
+CPSC 298-01
+Assignment 2
+
+This program reads integers into an array and prints them back out.
+
+*/
+
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+int main(int argc, char **argv) {
+  //member variables
+   string userStr[10];
+   int i,j;
+
+  //prompt user for string input
+  for (j = 0; j < 10; j++){
+   cout << "Enter a number: ";
+   cin >> userStr[j];
+  }
+
+  cout << endl;
+
+  // Print string back one at a time
+  for (i = 0; i < sizeof(userStr)/sizeof(userStr[0]); ++i){ //dividing by the size of each element
+  cout << "userStr[" << i << "] = " << userStr[i] << endl;
+  }
+ return 0;
+}
